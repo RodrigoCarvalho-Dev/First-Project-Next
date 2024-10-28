115ba99dce2c004716cca8e7c6d2a9198a6a29df
Rodrigo Carvalho Nogueira júnior <rodrigosms103042r6@gmail.com>
2024-10-28 11:59:15 -0300

    final project of next introductions

--- src/app/(auth)/product/[...data]/page.tsx
+++ src/app/(auth)/product/[...data]/page.tsx
@@ -30,7 +30,7 @@ export default async function Product({ params } : ProductProps ){
             <br />  
 
                 <AddToCard>
-                <Test/>
+                <Test/> 
                 </AddToCard>
         </h1>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,39 @@
+'use client';
 
-
+import { Suspense } from 'react';
 import { useEffect , useState } from 'react';
 import Header from './components/header';
+import GithubProfile from './components/profile/github-profile';
+import LongWaitComponent from './components/profile/long-wait-component';
 // Server Components => A gente não usa javascript no lado do Cliente
 // Client Components => A gente usa javascript no lado do servidor e possuem interatividade com o usuário
 
 // Streaming SSR => Ler/Escrever dados de forma parcial + Server-side Rendering
 // Renderizar um componente pelo lado do servidor de forma parcial 
 
-export default async function Home() {
-
-  const response_user = await fetch('https://api.github.com/users/diego3g');
-  const user = await response_user.json();
+export default function Home() {
 
   return (
-    <>
-          <pre>
-            {user.login}
-          </pre>
-    </>
-  );
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero aspernatur rerum hic necessitatibus, soluta molestiae, eos est natus porro incidunt doloribus quam alias, quasi voluptates facere accusamus animi et? Magni.
+      </p>
+    
+    <Suspense fallback={<p>Carregando LongWaitComponent...</p>}>
+    <LongWaitComponent/>
+    </Suspense>
+      
+    <Suspense fallback={
+      <p>
+        Carregando GitHub Profile...
+      </p>
+    }>
+    <GithubProfile/>
+    </Suspense>
+      
+    </div>
+  )
 }
+
+// Suspense => gerencia quais components vão ser renderizados na tela primeiro junto com um loading personalizado
